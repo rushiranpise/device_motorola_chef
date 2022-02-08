da26da95c117f329e703e83a364972075cfa06a2
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-02-08 18:29:58 +0530

    Build and enable fastbootd

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -16,6 +16,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
 
+# fastbootd
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fastbootd.available=true
+    
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.fm.use_audio_session=true
