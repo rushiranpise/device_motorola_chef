386a333c4d6d50824daa5faf1cb194811d937ae9
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-02-10 18:34:22 +0530

    Enable quick tap

--- aosp_chef.mk
+++ aosp_chef.mk
@@ -27,6 +27,7 @@ $(call inherit-product, device/motorola/chef/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # A/B updater
