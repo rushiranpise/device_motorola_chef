55bb308701544c80fad112f086dd32c7a95eb68a
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-02-17 19:21:03 +0530

    potato

--- potato_chef.mk
+++ potato_chef.mk
@@ -23,12 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Device
 $(call inherit-product, device/motorola/chef/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common POSP stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 # A/B updater
 AB_OTA_UPDATER := true
@@ -79,7 +79,7 @@ TARGET_RECOVERY_FSTAB := device/motorola/sdm660-common/rootdir/etc/fstab.qcom
 
 # Device identifiers
 PRODUCT_DEVICE := chef
-PRODUCT_NAME := aosp_chef
+PRODUCT_NAME := potato_chef
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := One Power
 PRODUCT_MANUFACTURER := Motorola
