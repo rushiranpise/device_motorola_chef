b32ce43a710baf50260b9e1d0f17c3d84d891204
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-05-18 15:16:06 +0530

    Update aosp_chef.mk

--- aosp_chef.mk
+++ aosp_chef.mk
@@ -27,7 +27,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_STOCK_ARCORE := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Quick Tap
